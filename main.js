07c157e7bbba678a33d439822c47ba97efd036f4
dev.graphikomsen <dev.graphikomsen@gmail.com>
2026-04-13 11:57:04 +0000

    Menu de navigation: html - js commit

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const navMenu = document.getElementById('Nav-menu');
+const navLink = document.querySelectorAll('.nav-link');
+const hamburger = document.getElementById('hamburger');
 
+hamburger.addEventListener('click', () => {
+
+    navMenu.classList.toggle('left-[0]');
+    hamburger.classList.toggle('ri-close-large-line');
+});
+
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+
+        navMenu.classList.toggle('left-[0]');
+        hamburger.classList.toggle('ri-close-large-line');
+    });
+});
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
 
